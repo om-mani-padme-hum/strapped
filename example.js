7c0ff28fecdd37bedcbbae78abb280eb9308b84b
Rich Lowe <rich.fb.email@gmail.com>
2019-01-10 14:31:55 -0600

    Added select department to example horizontal form

--- example.js
+++ example.js
@@ -248,6 +248,7 @@ app.get('/', (req, res, next) => {
   p.input('inputSet').id('male').name('sex').type('radio').label('Male').selected(true);
   p.input('inputSet').id('female').name('sex').type('radio').label('Female');
   p.select().id('department').name('department').label('Department:').labelCols(4).inputCols(6).horizontal(true);
+  p.option().value('').text('Select Department');
   p.option().value('engineering').text('Engineering');
   p.option().value('quality').text('Quality').selected(true);
   p.option().value('materials').text('Materials');
