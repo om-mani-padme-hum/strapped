fe1014b8d84fdbe3548a2104edea570555bfc0ff
Rich <rich.fb.email@gmail.com>
2019-06-17 16:49:05 -0700

    Changed method for setting modal size, this is a BREAKING CHANGE

--- modal.js
+++ modal.js
@@ -16,9 +16,7 @@ class Modal extends html.Div {
     this.init(data);
     
     this.middle(data.middle || false);
-    this.large(data.large || false);
-    this.small(data.small || false);
-    this.xlarge(data.xlarge || false);
+    this.size(data.size || ``);
   }
   
   /**
@@ -45,69 +43,23 @@ class Modal extends html.Div {
   }
   
   /**
-   * Large boolean getter/setter.
-   * @signature large() Get the large boolean
-   * @signature large(boolean) Set the large boolean as (boolean)
+   * Size getter/setter.
+   * @signature size() Get the size
+   * @signature size(string) Set the size as (string)
    * @return This object for call chaining
    */
-  large(arg1) {
+  size(arg1) {
     /** Getter */
     if ( arg1 === undefined )
-      return this._large;
+      return this._size;
     
     /** Setter */
-    else if ( typeof arg1 == 'boolean' )
-      this._large = arg1; 
+    else if ( typeof arg1 == 'string' )
+      this._size = arg1; 
     
     /** Handle errors */
     else
-      throw new TypeError(`Modal.large(): Invalid signature (${typeof arg1}).`);
-    
-    /** Allow for call chaining */
-    return this;
-  }
-  
-  /**
-   * Small boolean getter/setter.
-   * @signature small() Get the small boolean
-   * @signature small(boolean) Set the small boolean as (boolean)
-   * @return This object for call chaining
-   */
-  small(arg1) {
-    /** Getter */
-    if ( arg1 === undefined )
-      return this._small;
-    
-    /** Setter */
-    else if ( typeof arg1 == 'boolean' )
-      this._small = arg1; 
-    
-    /** Handle errors */
-    else
-      throw new TypeError(`Modal.small(): Invalid signature (${typeof arg1}).`);
-    
-    /** Allow for call chaining */
-    return this;
-  }
-  
-  /**
-   * XLarge boolean getter/setter.
-   * @signature xlarge() Get the xlarge boolean
-   * @signature xlarge(boolean) Set the xlarge boolean as (boolean)
-   * @return This object for call chaining
-   */
-  xlarge(arg1) {
-    /** Getter */
-    if ( arg1 === undefined )
-      return this._xlarge;
-    
-    /** Setter */
-    else if ( typeof arg1 == 'boolean' )
-      this._xlarge = arg1; 
-    
-    /** Handle errors */
-    else
-      throw new TypeError(`Modal.xlarge(): Invalid signature (${typeof arg1}).`);
+      throw new TypeError(`Modal.size(): Invalid signature (${typeof arg1}).`);
     
     /** Allow for call chaining */
     return this;
@@ -119,16 +71,15 @@ class Modal extends html.Div {
    * @return The rendered HTML
    */
   render(indent) {
+    if ( [`sm`, `lg`, `xl`, ``].includes(this.size()) )
+      throw new RangeError(`Modal.render(): Invalid size, must be 'sm', 'lg', or 'xl' if set.`);
+    
     const div = new html.Div().addClass(`modal fade`).attr('tabindex', '-1').attr('role', 'dialog').id(this.id());
     const div2 = new html.Div().addClass(`modal-dialog`).attr('role', 'document');
     const div3 = new html.Div();
     
-    if ( this.small() )
-      div2.addClass(`modal-sm`);
-    else if ( this.large() )
-      div2.addClass(`modal-lg`);
-    else if ( this.xlarge() )
-      div2.addClass(`modal-xl`);
+    if ( this.size().length > 0 )
+      div2.addClass(`modal-${this.size()}`);
     
     if ( this.middle() )
       div2.addClass(`modal-dialog-centered`);
