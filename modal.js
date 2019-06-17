c11150df9c88ebf2d56c47bf26ec9f460510806a
Rich <rich.fb.email@gmail.com>
2019-06-17 16:34:29 -0700

    Added modal size classes

--- modal.js
+++ modal.js
@@ -16,6 +16,9 @@ class Modal extends html.Div {
     this.init(data);
     
     this.middle(data.middle || false);
+    this.large(data.large || false);
+    this.small(data.small || false);
+    this.xlarge(data.xlarge || false);
   }
   
   /**
@@ -42,6 +45,75 @@ class Modal extends html.Div {
   }
   
   /**
+   * Large boolean getter/setter.
+   * @signature large() Get the large boolean
+   * @signature large(boolean) Set the large boolean as (boolean)
+   * @return This object for call chaining
+   */
+  large(arg1) {
+    /** Getter */
+    if ( arg1 === undefined )
+      return this._large;
+    
+    /** Setter */
+    else if ( typeof arg1 == 'boolean' )
+      this._large = arg1; 
+    
+    /** Handle errors */
+    else
+      throw new TypeError(`Modal.large(): Invalid signature (${typeof arg1}).`);
+    
+    /** Allow for call chaining */
+    return this;
+  }
+  
+  /**
+   * Small boolean getter/setter.
+   * @signature small() Get the small boolean
+   * @signature small(boolean) Set the small boolean as (boolean)
+   * @return This object for call chaining
+   */
+  small(arg1) {
+    /** Getter */
+    if ( arg1 === undefined )
+      return this._small;
+    
+    /** Setter */
+    else if ( typeof arg1 == 'boolean' )
+      this._small = arg1; 
+    
+    /** Handle errors */
+    else
+      throw new TypeError(`Modal.small(): Invalid signature (${typeof arg1}).`);
+    
+    /** Allow for call chaining */
+    return this;
+  }
+  
+  /**
+   * XLarge boolean getter/setter.
+   * @signature xlarge() Get the xlarge boolean
+   * @signature xlarge(boolean) Set the xlarge boolean as (boolean)
+   * @return This object for call chaining
+   */
+  xlarge(arg1) {
+    /** Getter */
+    if ( arg1 === undefined )
+      return this._xlarge;
+    
+    /** Setter */
+    else if ( typeof arg1 == 'boolean' )
+      this._xlarge = arg1; 
+    
+    /** Handle errors */
+    else
+      throw new TypeError(`Modal.xlarge(): Invalid signature (${typeof arg1}).`);
+    
+    /** Allow for call chaining */
+    return this;
+  }
+  
+  /**
    * Render the component.
    * @signature render(number) Render this component with (number) spaces of indentation
    * @return The rendered HTML
@@ -50,6 +122,13 @@ class Modal extends html.Div {
     const div = new html.Div().addClass(`modal fade`).attr('tabindex', '-1').attr('role', 'dialog').id(this.id());
     const div2 = new html.Div().addClass(`modal-dialog`).attr('role', 'document');
     const div3 = new html.Div();
+    
+    if ( this.small() )
+      div.addClass(`modal-sm`);
+    else if ( this.large() )
+      div.addClass(`modal-lg`);
+    else if ( this.xlarge() )
+      div.addClass(`modal-xl`);
     
     if ( this.middle() )
       div2.addClass(`modal-dialog-centered`);
